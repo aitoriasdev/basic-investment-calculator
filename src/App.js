9592e2184f8b88a7df31d7ba1c0255a3ff4f6276
Aitorias <203893417+aitoriasdev@users.noreply.github.com>
2026-01-30 11:06:41 +0000

    feature: Added header, calculator and result components to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import Calculator from "./features/calculator/calculator";
+import Header from "./features/header/header";
+import Result from "./features/result/result";
+import { calculateInvestmentResults } from "./util/investment.js";
+
+const INITIAL_INVESTMENTS = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
+
 function App() {
+  const [userInvestments, setUserInvestments] = useState(INITIAL_INVESTMENTS);
+
+  const data = calculateInvestmentResults(userInvestments);
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <main>
+        <Calculator inputInvestments={userInvestments} />
+        <Result results={data} />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
