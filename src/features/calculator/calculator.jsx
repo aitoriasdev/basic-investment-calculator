30b752a2125b8f7c18aa41594d0130f7a22a5e1d
Aitorias <203893417+aitoriasdev@users.noreply.github.com>
2026-01-30 11:34:24 +0000

    feature: Added computed state management from upper component in calculator component

--- src/features/calculator/calculator.jsx
+++ src/features/calculator/calculator.jsx
@@ -1,16 +1,41 @@
 import Input from "./input";
 
-export default function Calculator({ inputInvestments }) {
+export default function Calculator({ inputInvestments, onInputChange }) {
   const { initialInvestment, annualInvestment, expectedReturn, duration } =
     inputInvestments;
-    
+
+  function handleInputChange(inputName, newValue) {
+    onInputChange(inputName, newValue);
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
-        <Input label="Initial Investment" value={initialInvestment} />
-        <Input label="Annual Investment" value={annualInvestment} />
-        <Input label="Expected return" value={expectedReturn} />
-        <Input label="Duration" value={duration} />
+        <Input
+          label="Initial Investment"
+          name="initialInvestment"
+          value={initialInvestment}
+          onValueChange={handleInputChange}
+        />
+        <Input
+          label="Annual Investment"
+          name="annualInvestment"
+          value={annualInvestment}
+          onValueChange={handleInputChange}
+        />
+        <Input
+          label="Expected return"
+          name="expectedReturn"
+          step="0.1"
+          value={expectedReturn}
+          onValueChange={handleInputChange}
+        />
+        <Input
+          label="Duration"
+          name="duration"
+          value={duration}
+          onValueChange={handleInputChange}
+        />
       </div>
     </section>
   );
