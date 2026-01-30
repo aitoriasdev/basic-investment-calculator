481609ff0d7172464e9484dc5bc78c02c0e46bbc
Aitorias <203893417+aitoriasdev@users.noreply.github.com>
2026-01-30 11:05:15 +0000

    feature: Create header component to display the logo and the title

---
+++ src/features/header/header.jsx
@@ -0,0 +1,10 @@
+import img from "../../assets/investment-calculator-logo.png";
+
+export default function Header() {
+  return (
+    <header id="header">
+      <img alt="React Investment calculator" src={img} />
+      <h1>React Investment Calculator</h1>
+    </header>
+  );
+}
